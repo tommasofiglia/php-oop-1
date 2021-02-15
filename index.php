502513eb7b58996a0637565434832be3d545e5c4
Tommaso Figlia <tommasofiglia@gmail.com>
2021-02-15 16:45:40 +0100

    Piccole modifiche alle proprietà

--- index.php
+++ index.php
@@ -13,15 +13,16 @@ class Car {
   public $modelYear;
   public $year;
   public $color;
-  public $bodyStyle;
+  public $engine;
   public $alimentation;
   public $euro;
   public $price;
+  public $kilometers;
   public $productionCountry;
 
 // Uso il constructor per creare le proprietà con cui popolare la classe di istanze
 
-  function __constructor(string $brand, string $model, string $class, int $modelYear, int $year, string $color, string $bodyStyle, string $alimentation, int $euro, int $price, string $productionCountry){
+  function __constructor(string $brand, string $model, string $class, int $modelYear, int $year, string $color, float $engine, string $alimentation, int $euro, int $price, int $kilometers, string $productionCountry){
 
   $this->brand = $brand;
   $this->model = $model;
@@ -29,13 +30,22 @@ class Car {
   $this->modelYear = $modelYear;
   $this->year = $year;
   $this->color = $color;
-  $this->bodyStyle = $bodyStyle;
+  $this->engine = $engine;
   $this->alimentation = $alimentation;
   $this->euro = $euro;
   $this->price = $price;
-  $this->produc = $productionCountry;
+  $this->kilometers = $kilometers;
+  $this->productionCountry = $productionCountry;
 
   }
 }
 
- ?>
+// Creo delle istanze e le popolo
+$cars = [
+  'mx5' => new Fruit('mazda', 'mx5', 'S(roadster)', 2015, 2018, 'red', 1.5, 'petrol', 6, 18000, 0, 'japan'),
+  'fiesta' => new Fruit('ford', 'fiesta', 'B(supermini)', 2008, 2013, 'blue', 1.0, 'petrol', 5, 11000, 26000, 'usa' )
+];
+
+var_dump($cars);
+
+?>
